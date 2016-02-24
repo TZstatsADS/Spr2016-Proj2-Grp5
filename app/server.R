5176381ac82e7a3c8bb1a7aba7c23f33c3c8b7d9
Tara Shui <jumpinginwater@gmail.com>
2016-02-24 00:39:27 -0500

    Cleaned up code

--- app/server.R
+++ app/server.R
@@ -1,38 +1,48 @@
-#import all library
+# Import all library
 library(shiny)
 library(datasets)
 library(leaflet)
 library(data.table)
 library(dplyr)
 
-#import filtered data
-setwd("C:/Users/ouwen/Downloads")
-yellowdata<-fread('yellowfiltered.csv')
+# Import filtered data
+yellowdata<-fread('../data/yellowfiltered.csv')
 
-point = makeIcon("blue.png", 13, 13)
+point = makeIcon("../doc/blue.png", 13, 13)
 
-#began server code
+# Begin server code
 shinyServer(function(input, output){
   intdate <- reactive({
-    intdate <- as.POSIXct(paste(as.character(input$Day[1]), as.character(input$IntHour)), format = "%Y-%m-%d %H")
+    intdate <- as.POSIXct(paste(as.character(input$Day[1]), 
+                                as.character(input$IntHour)), 
+                          format = "%Y-%m-%d %H")
     })
   enddate <- reactive({
-    enddate <- as.POSIXct(paste(as.character(input$Day[2]), as.character(input$EndHour)), format = "%Y-%m-%d %H")
+    enddate <- as.POSIXct(paste(as.character(input$Day[2]), 
+                                as.character(input$EndHour)), 
+                          format = "%Y-%m-%d %H")
   })
   
-  #filter data   
+  # Filter data   
   temp <- reactive({
-    temp <- yellowdata %>%
-      filter(tpep_pickup_datetime >= intdate(), tpep_pickup_datetime <= enddate())
+    temp <- yellowdata %>% filter(tpep_pickup_datetime >= intdate(), 
+                                  tpep_pickup_datetime <= enddate())
   })
   
   amount <- reactive({
     amount <- input$Amount
   })
 
-  #draw a map
+  # Draw map
   output$map <- renderLeaflet({
-     leaflet(data = temp()[c(sample(1:dim(temp())[1], amount())),])%>% addTiles() %>% setView(lng=-73.9619,lat=40.8075,zoom=14)%>%
-       addMarkers(~dropoff_longitude, ~dropoff_latitude, icon= point, popup = ~ as.character(paste("Fare", fare_amount, "Tip", tip_amount, "Ratio", round(tip_amount/fare_amount, 3))))
+     leaflet(data = temp()[c(sample(1:dim(temp())[1], amount())),]) %>% 
+      addTiles() %>% 
+      setView(lng = -73.971035, lat = 40.775659, zoom = 12) %>% 
+      addMarkers(~dropoff_longitude, ~dropoff_latitude, 
+                 icon= point, 
+                 popup = ~ as.character(paste("Fare: ", fare_amount, 
+                                              "Tip: ", tip_amount, 
+                                              "Ratio: ", round(tip_amount/fare_amount, 3)))
+                 )
   })
 })
--- app/ui.r
+++ app/ui.r
@@ -2,22 +2,21 @@ library(ggvis)
 library(leaflet)
 
 shinyUI(fluidPage(
-        titlePanel("Life, out of Campus"),
-        
-        sidebarLayout(position = "right",
-                       sidebarPanel(
+        titlePanel("When Columbia Students Leave the Bubble..."),
+        sidebarLayout(position = "right", 
+                      sidebarPanel(
                                h4("Filter"),
-                               dateRangeInput("Day", "someday in February", start = "2015-02-01", end = "2015-02-28", min = "2015-02-01", max = "2015-02-28"),
-                               sliderInput("IntHour", "Hour of the first Day", 0, 24, 0, step = 1),
-                               sliderInput("EndHour", "Hour of the last Day", 0, 24, 0, step =1),
-                               sliderInput("Amount", "Amount of trips to display", 1, 1000, 100, step =10),
+                               dateRangeInput("Day", "Choose a date range in February", 
+                                              start = "2015-02-14", end = "2015-02-14", 
+                                              min = "2015-02-01", max = "2015-02-28"),
+                               sliderInput("IntHour", "Start time (of the first date)", 0, 24, 9, step = 1),
+                               sliderInput("EndHour", "End time (of the last date)", 0, 24, 24, step = 1),
+                               sliderInput("Amount", "Number of trips to display", 1, 1000, 50, step =10),
                                submitButton("Submit"),
                                style = "opacity : 0.85"
                                ),
                       mainPanel(
                               leafletOutput("map", width = "150%", height = 700)
-                                )
-
-        )
-)
-)
+                              )
+                      )
+))
